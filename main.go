e90c08ea9407e826302c2bbf4f86cb99ca0230ae
Issy Long <me@issyl0.co.uk>
2020-07-05 18:09:10 +0100

    Put the data structures outside of the functions they're used in
    - I'll probably want to use the Accounts one elsewhere when this
      supports getting balances.

--- main.go
+++ main.go
@@ -7,6 +7,35 @@ import (
 	"os"
 )
 
+// Accounts implements the data structure of the `/accounts` Monzo API response.
+// Docs at https://docs.monzo.com/#accounts.
+type Accounts struct {
+	Accounts []struct {
+		ID          string `json:"id"`
+		Closed      bool   `json:"closed"`
+		Created     string `json:"created"`
+		Description string `json:"description"`
+		Type        string `json:"type"`
+		Currency    string `json:"currency"`
+		CountryCode string `json:"country_code"`
+		Owners      []struct {
+			UserID             string `json:"user_id"`
+			PreferredName      string `json:"preferred_name"`
+			PreferredFirstName string `json:"preferred_first_name"`
+		} `json:"owners"`
+		AccountNumber int `json:"account_number"`
+		SortCode      int `json:"sort_code"`
+	} `json:"accounts"`
+}
+
+// Ping implements the data structure for the `/ping/whoami` Monzo API response.
+// Docs at https://docs.monzo.com/#authenticating-requests.
+type Ping struct {
+	Authenticated bool   `json:"authenticated"`
+	ClientID      string `json:"client_id"`
+	UserID        string `json:"user_id"`
+}
+
 func main() {
 	const monzoAPI = "https://api.monzo.com"
 
@@ -21,12 +50,6 @@ func main() {
 }
 
 func checkMonzoTokenWorks(apiToken string, monzoAPI string) bool {
-	type Ping struct {
-		Authenticated bool   `json:"authenticated"`
-		ClientID      string `json:"client_id"`
-		UserID        string `json:"user_id"`
-	}
-
 	client := resty.New()
 	resp, err := client.R().SetResult(&Ping{}).SetAuthToken(apiToken).Get(monzoAPI + "/ping/whoami")
 
@@ -47,25 +70,6 @@ func checkMonzoTokenWorks(apiToken string, monzoAPI string) bool {
 }
 
 func getUserDetails(apiToken string, monzoAPI string) string {
-	type Accounts struct {
-		Accounts []struct {
-			ID          string `json:"id"`
-			Closed      bool   `json:"closed"`
-			Created     string `json:"created"`
-			Description string `json:"description"`
-			Type        string `json:"type"`
-			Currency    string `json:"currency"`
-			CountryCode string `json:"country_code"`
-			Owners      []struct {
-				UserID             string `json:"user_id"`
-				PreferredName      string `json:"preferred_name"`
-				PreferredFirstName string `json:"preferred_first_name"`
-			} `json:"owners"`
-			AccountNumber int `json:"account_number"`
-			SortCode      int `json:"sort_code"`
-		} `json:"accounts"`
-	}
-
 	client := resty.New()
 	resp, err := client.R().SetAuthToken(apiToken).Get(monzoAPI + "/accounts")
 
